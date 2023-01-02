c590233ea1b4209f9cbd52a0b04233e78721a1b7
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-02 09:28:45 -0700

    Adds migrations for actors and movie actors tables

---
+++ db/migrate/20230102161658_create_table_actors.rb
@@ -0,0 +1,9 @@
+class CreateTableActors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :actors do |t|
+      t.string :name
+      t.integer :age
+      t.timestamp
+    end
+  end
+end
