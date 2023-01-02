df54b506285906566029f9d4ff511e34cf933165
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-02 11:14:41 -0700

    Adds #actors_alpha to movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def actors_alpha
+    actors.order(:name).to_a
+  end
 end
